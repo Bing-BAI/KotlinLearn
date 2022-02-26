86e3f40041601ad70ef0e5b0f6dad85f9cddc7bf
bing <bai224bing2464@gmail.com>
2022-02-27 02:34:19 +0900

    impl dice imgs roll

--- app/src/main/java/com/example/kotlinlearn/MainActivity.kt
+++ app/src/main/java/com/example/kotlinlearn/MainActivity.kt
@@ -3,12 +3,17 @@ package com.example.kotlinlearn
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import kotlin.random.Random as Random
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var diceImage: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.roll_button)
@@ -23,13 +28,22 @@ class MainActivity : AppCompatActivity() {
             ).show()**/
         }
 
+        diceImage = findViewById(R.id.dice_image)
 
     }
 
     private fun rollDice() {
 
-        val resultText: TextView = findViewById(R.id.result_text)
-        val randomInt = Random.nextInt(6) + 1
-        resultText.text = randomInt.toString()
+        val drawableResource = when (Random.nextInt(6) + 1) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(drawableResource)
+
     }
 }
